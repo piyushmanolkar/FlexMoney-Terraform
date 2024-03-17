178d7a6bd23c4070eb00319d5d71224a27d52ed4
Piyush <manolkarpiyush@gmail.com>
2024-03-17 20:26:02 +0530

    Fixing Error

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "ami_instance_id" {
   description = "ID of the AMI Server"
-  value = aws_ec2_instance_state.ec2_instance.id
+  value = aws_instance.ec2_instance.id
 }
 
 output "load_balancer_url" {
